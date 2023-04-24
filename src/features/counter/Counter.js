e437b281d5de577c2219565f1ec0f35c698ce944
Bruno Jacby <brunojck@hotmail.com>
2023-04-24 22:31:45 +0000

    adc redux buttons

---
+++ src/features/counter/Counter.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './counterSlice'
+
+export function Counter() {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <div>
+        <button
+          className='btn fourth'
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+            className='btn fourth'
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  )
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import { store } from '../app/store'
+import { Provider } from 'react-redux'
+import { Counter } from '@/features/counter/Counter'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <Provider store={store}>
+    <Counter />
+  </Provider>
+  )
 }
